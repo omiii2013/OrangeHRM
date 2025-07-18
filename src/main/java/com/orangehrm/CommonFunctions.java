9b913b40c522f25fd5a60e32baa3172b05fcf4b0
Omiii <otank015@gmail.com>
2025-07-18 01:47:21 -0400

    UPDATE: Add new common functionalities setValue, OnClick, getWebElement and isElementDisplayed
    Introduced utility methods in CommonFunctions for interacting with web elements, including methods to get single/multiple elements, set values, click, and check visibility.
    
    CommonFunctions extend TestBase class and will be inherited by different modules like Login, HRAdministration and other too.

--- src/main/java/com/orangehrm/CommonFunctions.java
+++ src/main/java/com/orangehrm/CommonFunctions.java
@@ -1,5 +1,56 @@
 package com.orangehrm;
 
-public class CommonFunctions {
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.orangehrm.base.BaseClass;
+
+public class CommonFunctions extends BaseClass{
 	 
+	/**
+	 * Get the single WebElement
+	 */
+	public WebElement getWebElement(By by) {
+		
+		return driver.findElement(by);
+	}
+	
+	/**
+	 * Get the list of WebElements 
+	 * @param by as element locator
+	 * @return
+	 */
+	public List<WebElement> getWebElements(By by) {
+		
+		return driver.findElements(by);
+	}
+	
+	/**
+	 * Enter the value in the textbox
+	 */
+	public void setValue(By by, String value) {
+		
+		driver.findElement(by).sendKeys(value);
+	}
+	
+	/**
+	 * Click method
+	 * @param by as element locator
+	 */
+	public void onClick(By by) {
+		
+		driver.findElement(by).click();
+	}
+	
+	/**
+	 * Find out if element is displayed in page or not
+	 * @param by as element locator
+	 * @return element is displayed or not
+	 */
+	public boolean isElementDisplayed(By by) {
+		
+		return driver.findElement(by).isDisplayed();
+	}
 }
